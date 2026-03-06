dd5b9cb1dc80a9fef978d3f9dab90ab4f28cc937
dawcio1579-creator <dawcio1579@gmail.com>
2026-03-06 20:38:13 +0100

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:37:51.441Z
-Commit: Add dark mode support
+Timestamp: 2026-03-06T19:38:01.481Z
+Commit: Resolve null pointer exception in user service
