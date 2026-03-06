53ca7ffb807337f6af8d83d1f0b72a654eea887c
dawcio1579-creator <dawcio1579@gmail.com>
2026-03-06 20:40:59 +0100

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:40:46.135Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T19:40:47.632Z
+Commit: Resolve timezone issues in date handling
